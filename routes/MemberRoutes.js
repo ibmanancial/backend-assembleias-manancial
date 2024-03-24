3763abdeb94470e399c3b7407a9d862acbbfa553
UrielBatista <ur.sabatista@gmail.com>
2024-03-24 00:06:01 -0300

    🚧 Adding new route to create activities. WIP

--- routes/MemberRoutes.js
+++ routes/MemberRoutes.js
@@ -2,11 +2,13 @@ import {Router} from "express";
 import middlewareMember from "../middlewares/middlewares.js";
 import middlewareAuthentication from "../middlewares/authenticationMiddleware.js";
 import MemberController from "../controllers/MemberController.js";
+import ActivityController from "../controllers/ActivityController.js";
 
 const router = Router();
 
 router.post("/member", middlewareAuthentication.checkAdminPermitionRole, middlewareMember.verifyNewMember, MemberController.createManancialMember)
 router.post("/qualificationsMember", middlewareAuthentication.checkAdminPermitionRole, middlewareMember.checkQualificationsMember, MemberController.createMemberQualification)
+router.post("/activitiesManancial", middlewareAuthentication.checkMidiaPermitionRole, middlewareMember.checkActivitiesManancial, ActivityController.createManancialActivity)
 
 router.get("/getMember", middlewareAuthentication.checkAnyPermitionsRoles, MemberController.requestManancialMember)
 router.get("/getAllMembers", middlewareAuthentication.checkAnyPermitionsRoles, MemberController.requestAllManancialMember)
